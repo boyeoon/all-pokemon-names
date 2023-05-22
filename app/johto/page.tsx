8b0cd37b5d9e73f7569032d8d483086602087b74
BoYeonJang <uko010199@gmail.com>
2023-05-22 15:19:38 +0900

    add pokemon region

--- app/johto/page.tsx
+++ app/johto/page.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 
 const mainText = "마리의 포켓몬을 전부 알고 있나요?";
 
-export default function Home() {
+// 성도 지방
+export default function Johto() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
--- app/unova/page.tsx
+++ app/unova/page.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 
 const mainText = "마리의 포켓몬을 전부 알고 있나요?";
 
-export default function Home() {
+// 하나 지방
+export default function Unova() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
