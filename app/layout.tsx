8e29eedeefab87d729857523fc829c9de48d8995
Boyeon Jang <uko010199@gmail.com>
2024-10-30 04:51:16 +0900

    chore: update header and footer paths

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,8 @@
 import type { Metadata } from "next";
+
+import Header from "@/components/header/page";
+import Footer from "@/components/footer/page";
+
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -15,7 +19,9 @@ export default function RootLayout({
       <body
       // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Header />
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
       <main>
         <h1>all pokemon names</h1>
+        <h2>fdsf</h2>
       </main>
-      <footer></footer>
     </div>
   );
 }
