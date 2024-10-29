ca69852d0d99384053d437dea038777fc26e911f
Boyeon Jang <uko010199@gmail.com>
2024-10-30 05:00:52 +0900

    feat: add title text

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,14 @@ export default function Home() {
   return (
     <div>
       <main>
-        <h1>all pokemon names</h1>
-        <h2>fdsf</h2>
+        <div>
+          <p className="m-8 text-4xl font-bold text-center">
+            <ruby>
+              모든 지방<rt className="tracking-[.5em]">1-9세대</rt>
+            </ruby>
+            &nbsp;1511마리의 포켓몬을 전부 알고있나요?
+          </p>
+        </div>
       </main>
     </div>
   );
