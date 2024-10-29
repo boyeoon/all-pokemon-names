ecfd0cd773e8cf99c8872a80998b1fcc72748b2c
Boyeon Jang <uko010199@gmail.com>
2024-10-30 05:01:40 +0900

    feat: add kanto region

---
+++ app/regions/kanto/page.tsx
@@ -0,0 +1,12 @@
+export default function Kanto() {
+  return (
+    <div>
+      <p className="m-8 text-4xl font-bold text-center">
+        <ruby>
+          관동 지방<rt className="tracking-[.5em]">1세대</rt>
+        </ruby>
+        &nbsp;151마리의 포켓몬을 전부 알고있나요?
+      </p>
+    </div>
+  );
+}
