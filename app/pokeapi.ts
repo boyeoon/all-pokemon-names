cf1a53ba627a6920edc18c153ee9c65a0d55c42e
Boyeon Jang <uko010199@gmail.com>
2024-11-01 04:29:22 +0900

    feat: add PokeAPI

---
+++ app/pokeapi.ts
@@ -0,0 +1,26 @@
+export interface PokeAPI {
+  id: number;
+  name: string;
+  sprites: {
+    front_default: string;
+  };
+}
+
+export const fetchPokemonData = async (id: number): Promise<PokeAPI> => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  if (!response.ok) {
+    throw new Error("포켓몬 데이터를 가져오는 데 문제가 발생했습니다.");
+  }
+  const data = await response.json();
+
+  const koreanName =
+    data.names?.find(
+      ({ language }: { language: { name: string } }) => language.name === "ko"
+    )?.name || "이름을 찾을 수 없음";
+
+  return {
+    id: data.id,
+    name: koreanName,
+    sprites: data.sprites,
+  };
+};
