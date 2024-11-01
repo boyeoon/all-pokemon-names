cb58bec457cc1e131cdae9f4cba02a0aa0e30efa
Boyeon Jang <uko010199@gmail.com>
2024-11-01 18:12:47 +0900

    fix: update kanto region

--- app/regions/kanto/page.tsx
+++ app/regions/kanto/page.tsx
@@ -1,12 +1,18 @@
+import PokemonQuiz from "@/components/quiz/page";
+
 export default function Kanto() {
   return (
     <div>
-      <p className="m-8 text-4xl font-bold text-center">
+      <p className="m-16 text-4xl font-bold text-center">
         <ruby>
-          관동 지방<rt className="tracking-[.5em]">1세대</rt>
+          관동 지방
+          <rt className="tracking-[.5em] text-primary">1세대</rt>
         </ruby>
-        &nbsp;151마리의 포켓몬을 전부 알고있나요?
+        &nbsp;
+        <span className="rounded bg-yellow-500/30 text-primary">151마리</span>의
+        포켓몬을 전부 알고있나요?
       </p>
+      <PokemonQuiz numPokemonsStr={1} numPokemonsEnd={151} />
     </div>
   );
 }
