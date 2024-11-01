89c362ded6842e90d3f033b39a163f32079a9914
Boyeon Jang <uko010199@gmail.com>
2024-11-01 17:35:20 +0900

    feat: display pokemon names in Korean
    - Updated to display Pokémon names in Korean.

--- app/pokeapi.ts
+++ app/pokeapi.ts
@@ -13,8 +13,16 @@ export const fetchPokemonData = async (id: number): Promise<PokeAPI> => {
   }
   const data = await response.json();
 
+  const koreanResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon-species/${id}`
+  );
+  if (!koreanResponse.ok) {
+    throw new Error("포켓몬 이름을 가져오는 데 문제가 발생했습니다.");
+  }
+  const koranData = await koreanResponse.json();
+
   const koreanName =
-    data.names?.find(
+    koranData.names?.find(
       ({ language }: { language: { name: string } }) => language.name === "ko"
     )?.name || "이름을 찾을 수 없음";
 
