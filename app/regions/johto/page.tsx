c2663c0e86ab55b0767085061015e026be6244ab
Boyeon Jang <uko010199@gmail.com>
2024-11-01 18:13:01 +0900

    feat: add johto region

---
+++ app/regions/johto/page.tsx
@@ -0,0 +1,18 @@
+import PokemonQuiz from "@/components/quiz/page";
+
+export default function Kanto() {
+  return (
+    <div>
+      <p className="m-16 text-4xl font-bold text-center">
+        <ruby>
+          성동 지방
+          <rt className="tracking-[.5em] text-primary">2세대</rt>
+        </ruby>
+        &nbsp;
+        <span className="rounded bg-yellow-500/30 text-primary">100마리</span>의
+        포켓몬을 전부 알고있나요?
+      </p>
+      <PokemonQuiz numPokemonsStr={152} numPokemonsEnd={251} />
+    </div>
+  );
+}
