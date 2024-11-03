288f2e124e1085df83537cae58bfe31afe5a5ff3
Boyeon Jang <uko010199@gmail.com>
2024-11-04 03:54:58 +0900

    feat: add sinnoh region

---
+++ app/regions/sinnoh/page.tsx
@@ -0,0 +1,20 @@
+import PokemonQuiz from "@/components/quiz/quiz";
+
+export default function Sinnoh() {
+  return (
+    <div>
+      <p className="m-16 text-4xl font-bold text-center">
+        <ruby>
+          신오 지방
+          <rt className="tracking-[.5em] text-primary">4세대</rt>
+        </ruby>
+        &nbsp;
+        <span className="underline rounded text-primary decoration-wavy decoration-3">
+          107마리
+        </span>
+        의 포켓몬을 전부 알고있나요?
+      </p>
+      <PokemonQuiz numPokemonsStr={387} numPokemonsEnd={493} />
+    </div>
+  );
+}
