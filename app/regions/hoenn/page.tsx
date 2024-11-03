7545e8224f6fe9b6f316690ece95417c64c67c5b
Boyeon Jang <uko010199@gmail.com>
2024-11-04 03:52:35 +0900

    feat: add hoenn region

---
+++ app/regions/hoenn/page.tsx
@@ -0,0 +1,20 @@
+import PokemonQuiz from "@/components/quiz/quiz";
+
+export default function Hoenn() {
+  return (
+    <div>
+      <p className="m-16 text-4xl font-bold text-center">
+        <ruby>
+          호연 지방
+          <rt className="tracking-[.5em] text-primary">3세대</rt>
+        </ruby>
+        &nbsp;
+        <span className="underline rounded text-primary decoration-wavy decoration-3">
+          135마리
+        </span>
+        의 포켓몬을 전부 알고있나요?
+      </p>
+      <PokemonQuiz numPokemonsStr={252} numPokemonsEnd={386} />
+    </div>
+  );
+}
