a3454d94d764a0e88de4b9ffbe96de6b78f9cba1
Boyeon Jang <uko010199@gmail.com>
2024-11-04 21:41:57 +0900

    feat: add paldea region

---
+++ app/regions/paldea/page.tsx
@@ -0,0 +1,20 @@
+import PokemonQuiz from "@/components/quiz/quiz";
+
+export default function Paldea() {
+  return (
+    <div>
+      <p className="m-16 text-4xl font-bold text-center">
+        <ruby>
+          팔데아 지방
+          <rt className="tracking-[.5em] text-primary">9세대</rt>
+        </ruby>
+        &nbsp;
+        <span className="underline rounded text-primary decoration-wavy decoration-3">
+          120마리
+        </span>
+        의 포켓몬을 전부 알고있나요?
+      </p>
+      <PokemonQuiz numPokemonsStr={906} numPokemonsEnd={1025} />
+    </div>
+  );
+}
