8bff5e5b1765c0452b1c75e52172350958f0cb7e
Boyeon Jang <uko010199@gmail.com>
2024-11-04 21:39:47 +0900

    feat: add galar region

---
+++ app/regions/galar/page.tsx
@@ -0,0 +1,20 @@
+import PokemonQuiz from "@/components/quiz/quiz";
+
+export default function Galar() {
+  return (
+    <div>
+      <p className="m-16 text-4xl font-bold text-center">
+        <ruby>
+          가라르 지방
+          <rt className="tracking-[.5em] text-primary">8세대</rt>
+        </ruby>
+        &nbsp;
+        <span className="underline rounded text-primary decoration-wavy decoration-3">
+          96마리
+        </span>
+        의 포켓몬을 전부 알고있나요?
+      </p>
+      <PokemonQuiz numPokemonsStr={810} numPokemonsEnd={905} />
+    </div>
+  );
+}
