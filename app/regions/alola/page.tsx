835506946551a8ec8311c97a6af17dcd9a9f4a75
Boyeon Jang <uko010199@gmail.com>
2024-11-04 21:37:23 +0900

    feat: add alola region

---
+++ app/regions/alola/page.tsx
@@ -0,0 +1,20 @@
+import PokemonQuiz from "@/components/quiz/quiz";
+
+export default function Alola() {
+  return (
+    <div>
+      <p className="m-16 text-4xl font-bold text-center">
+        <ruby>
+          알로라 지방
+          <rt className="tracking-[.5em] text-primary">7세대</rt>
+        </ruby>
+        &nbsp;
+        <span className="underline rounded text-primary decoration-wavy decoration-3">
+          88마리
+        </span>
+        의 포켓몬을 전부 알고있나요?
+      </p>
+      <PokemonQuiz numPokemonsStr={722} numPokemonsEnd={809} />
+    </div>
+  );
+}
