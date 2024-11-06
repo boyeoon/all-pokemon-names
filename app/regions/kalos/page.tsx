3148ffe40c3ff0e044b7f1e132dea25da1b32c26
Boyeon Jang <uko010199@gmail.com>
2024-11-06 17:10:06 +0900

    feat: import pokemon names for kalos region

--- app/regions/kalos/page.tsx
+++ app/regions/kalos/page.tsx
@@ -1,4 +1,5 @@
 import PokemonQuiz from "@/components/quiz/quiz";
+import { PokeNames } from "@/pokename";
 
 export default function Kalos() {
   return (
@@ -14,7 +15,11 @@ export default function Kalos() {
         </span>
         의 포켓몬을 전부 알고있나요?
       </p>
-      <PokemonQuiz numPokemonsStr={650} numPokemonsEnd={721} />
+      <PokemonQuiz
+        numPokemonsStr={650}
+        numPokemonsEnd={721}
+        regionPokemons={PokeNames.Kalos}
+      />
     </div>
   );
 }
