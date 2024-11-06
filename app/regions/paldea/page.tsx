819e88c361cfb735576355d995204626e182e530
Boyeon Jang <uko010199@gmail.com>
2024-11-06 17:11:56 +0900

    feat: import pokemon names for paldea region

--- app/regions/paldea/page.tsx
+++ app/regions/paldea/page.tsx
@@ -1,4 +1,5 @@
 import PokemonQuiz from "@/components/quiz/quiz";
+import { PokeNames } from "@/pokename";
 
 export default function Paldea() {
   return (
@@ -14,7 +15,11 @@ export default function Paldea() {
         </span>
         의 포켓몬을 전부 알고있나요?
       </p>
-      <PokemonQuiz numPokemonsStr={906} numPokemonsEnd={1025} />
+      <PokemonQuiz
+        numPokemonsStr={906}
+        numPokemonsEnd={1025}
+        regionPokemons={PokeNames.Paldea}
+      />
     </div>
   );
 }
